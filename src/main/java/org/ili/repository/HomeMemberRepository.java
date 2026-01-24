dc9d75ffafe596b9a1c517d08a94ab8b05b45edd
Cameron Delsol <cameron_delsol@ens.univ-artois.fr>
2026-01-24 20:23:02 +0100

    :bug: Fix HomeMemberRepository ID type to HomeMemberId

--- src/main/java/org/ili/repository/HomeMemberRepository.java
+++ src/main/java/org/ili/repository/HomeMemberRepository.java
@@ -1,6 +1,6 @@
 package org.ili.repository;
 
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.ili.entity.HomeMember;
 import org.ili.entity.HomeMemberId;
@@ -8,7 +8,7 @@ import org.ili.entity.HomeMemberId;
 import java.util.List;
 
 @ApplicationScoped
-public class HomeMemberRepository implements PanacheRepository<HomeMember> {
+public class HomeMemberRepository implements PanacheRepositoryBase<HomeMember, HomeMemberId> {
     public List<HomeMember> findByUserId(Long userId) {
         return find("user.id", userId).list();
     }
